1c27529a81150d6e3854b7f3bdfc8abcced6969b
Tony Joseph <tony@Tonys-Mac-mini.local>
2025-12-14 20:12:08 -0800

    Feat: Initialized Capacitor (Android)

---
+++ android/app/src/main/java/com/habitisim/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.habitisim.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.habitisim.app',
+  appName: 'Habitisim',
+  webDir: 'out'
+};
+
+export default config;
